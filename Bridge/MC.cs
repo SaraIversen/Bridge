8113f0cdc499ff860270aa26065ae106aee3b948
SaraIversen <sara.skovgaard.iversen@gmail.com>
2025-09-24 09:41:58 +0200

    Task3

---
+++ Bridge/MC.cs
@@ -0,0 +1,29 @@
+﻿namespace Bridge
+{
+    /// <summary>
+    /// Contains data about the specific MC.
+    /// </summary>
+    public class MC
+    {
+        public string LicensePlate { get; set; }
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Gets the price for this specific MC class.
+        /// </summary>
+        /// <returns>The price of the MC as a double</returns>
+        public double Price()
+        {
+            return 120;
+        }
+
+        /// <summary>
+        /// Gets the vehicle type for this specific MC class.
+        /// </summary>
+        /// <returns>The vecihcle type of the MC as a string</returns>
+        public string VehicleType()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ BridgeTests/MCTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Bridge.Tests
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass()]
+    public class MCTests
+    {
+        [DataTestMethod()]
+        [DataRow]
+        public void PriceTest()
+        {
+            // Arrange
+            MC mc = new MC();
+
+            // Act
+            double result = mc.Price();
+
+            // Assert
+            Assert.AreEqual(120, result, 0.001);
+        }
+
+        [DataTestMethod()]
+        [DataRow]
+        public void VehicleTypeTest()
+        {
+            // Arrange
+            MC mc = new MC();
+
+            // Act
+            string result = mc.VehicleType();
+
+            // Assert
+            Assert.AreEqual("MC", result);
+        }
+    }
+}
