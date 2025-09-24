0f4bb610a49bb1cd6002eab2c0692eb11c67181f
SaraIversen <sara.skovgaard.iversen@gmail.com>
2025-09-24 09:32:35 +0200

    Add project files.

---
+++ Bridge/Car.cs
@@ -0,0 +1,29 @@
+﻿namespace Bridge
+{
+    /// <summary>
+    /// Contains data about the specific car.
+    /// </summary>
+    public class Car
+    {
+        public string LicensePlate { get; set; }
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Gets the price for this specific car class.
+        /// </summary>
+        /// <returns>The price of the car as a double</returns>
+        public double Price()
+        {
+            return 230;
+        }
+
+        /// <summary>
+        /// Gets the vehicle type for this specific car class.
+        /// </summary>
+        /// <returns>The vecihcle type of the car as a string</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ BridgeTests/CarTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Bridge.Tests
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass()]
+    public class CarTests
+    {
+        [DataTestMethod()]
+        [DataRow]
+        public void PriceTest()
+        {
+            // Arrange
+            Car car = new Car();
+
+            // Act
+            double result = car.Price();
+
+            // Assert
+            Assert.AreEqual(230, result, 0.001);
+        }
+
+        [DataTestMethod()]
+        [DataRow]
+        public void VehicleTypeTest()
+        {
+            // Arrange
+            Car car = new Car();
+
+            // Act
+            string result = car.VehicleType();
+
+            // Assert
+            Assert.AreEqual("Car", result);
+        }
+    }
+}
